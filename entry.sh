782eb067e75c5308098f625915e6e88f2f78523a
Gamerx20003 <jeganramamurthyv@gmail.com>
2021-08-24 23:24:06 +0530

    all done

--- entry.sh
+++ entry.sh
@@ -1,17 +1,16 @@
 #! /bin/bash
 
-echo "Enter the option number of column: 1.'name' 2.'employeeId' 3.'phone' 4.'salaries' 5.'email' 6.'year' "
-read colno
-((colno++))
-
-echo "Enter parameter to replace:"
-read old
-
-echo "Enter parameter to replace it with:"
-read new
+colno="$1"
+old="$2"
+new="$3"
 
 while read line 
 do
-    echo ${line//$old/$new} 
+  if [[ $line =~ .*"$old".* ]]
+  then
+    echo ${line/$old/$new} 
+  else
+    echo $line
+  fi
 done < merged.txt > merged.txt.t 
 mv merged.txt{.t,}
